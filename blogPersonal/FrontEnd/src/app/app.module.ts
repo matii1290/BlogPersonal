3bc779c6eaf36256f6186e3e3cfe0a751888f891
matii1290 <101079021+matii1290@users.noreply.github.com>
2022-10-13 03:25:13 -0300

    Creacion de carpeta Frontend
    En esta sección creamos la carpeta frontend y hacemos el commit.

---
+++ blogPersonal/FrontEnd/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Navegador2Component } from './components/header/components/navegador2/navegador2.component';
+import { NoticiasComponent } from './components/noticias/noticias.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    Navegador2Component,
+    NoticiasComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blogPersonal/FrontEnd/src/app/components/header/components/navegador2/navegador2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navegador2',
+  templateUrl: './navegador2.component.html',
+  styleUrls: ['./navegador2.component.css']
+})
+export class Navegador2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
