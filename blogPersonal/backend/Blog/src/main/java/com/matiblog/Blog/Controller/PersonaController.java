72281d715e236e6d54c3bb711ef1b1752c2306d5
matii1290 <101079021+matii1290@users.noreply.github.com>
2022-10-27 15:14:08 -0300

    Update PersonaController.java

--- blogPersonal/backend/Blog/src/main/java/com/matiblog/Blog/Controller/PersonaController.java
+++ blogPersonal/backend/Blog/src/main/java/com/matiblog/Blog/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontblog1.web.app/")
 
 public class PersonaController {
 
