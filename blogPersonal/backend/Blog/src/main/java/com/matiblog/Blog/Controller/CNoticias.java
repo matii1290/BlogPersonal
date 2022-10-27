30e488dee52e1b69c0934d7f2bc3fc31f5e85316
matii1290 <101079021+matii1290@users.noreply.github.com>
2022-10-27 15:14:30 -0300

    Update CNoticias.java

--- blogPersonal/backend/Blog/src/main/java/com/matiblog/Blog/Controller/CNoticias.java
+++ blogPersonal/backend/Blog/src/main/java/com/matiblog/Blog/Controller/CNoticias.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("noticias")
-@CrossOrigin(origins = "http://localhost:4200/")
+@CrossOrigin(origins = "https://frontblog1.web.app/")
 public class CNoticias {
 
     @Autowired
